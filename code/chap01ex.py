4845ceb6417de7bd5f5e9236863b369fd54c44fd
John Lomas <jslomas@ucdavis.edu>
2018-09-25 16:44:29 -0700

    Done with Chap1

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -6,6 +6,7 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
 from __future__ import print_function
+from collections import defaultdict
 
 import numpy as np
 import sys
@@ -13,13 +14,38 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp2002(dct_file='2002FemResp.dct',
+                    dat_file='2002FemResp.dat.gz'):
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    nsfg.CleanFemResp(df)
+    return df
+
+def ReadFemPreg2002(dct_file='2002FemPreg.dct',
+                    dat_file='2002FemPreg.dat.gz'):            
+	dct = thinkstats2.ReadStataDct(dct_file)
+	df = dct.ReadFixedWidth(dat_file, compression='gzip')
+	return df
 
 def main(script):
-    """Tests the functions in this module.
+	"""Tests the functions in this module.
 
-    script: string script name
-    """
-    print('%s: All tests passed.' % script)
+	script: string script name
+	"""
+	resp2002 = ReadFemResp2002()
+	print(resp2002.pregnum.value_counts(sort=True))
+	CrazyPerson = resp2002[resp2002.pregnum==19]
+	preg = ReadFemPreg2002()
+	case19 = preg[preg.caseid==CrazyPerson.caseid[6528]]
+	case19.nbrnaliv.sum()
+	
+	
+	
+	
+	CasePreg = preg[preg.caseid == 6528]
+	print(CasePreg.nbrnaliv[1])
+	
+	print('%s: All tests passed.' % script)
 
 
 if __name__ == '__main__':
